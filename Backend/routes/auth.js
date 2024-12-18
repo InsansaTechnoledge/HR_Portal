753538d420393a9fc52146f3b57bcd38dd631a97
BHADRIKA <bhadrika2226@gmail.com>
2024-12-18 16:44:58 +0530

    Remove checkSession route from auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { login , checkSession, logout, getUser } from '../controller/authController.js'
+import { login , logout, getUser } from '../controller/authController.js'
 import checkCookies from '../middleware/checkCookies.js';
 
 const router = express.Router();
 
 router.post('/login', login);
-router.get('/checkSession',checkSession);
 router.post('/logout', logout);
 router.get('/checkCookies',checkCookies, getUser);
 
