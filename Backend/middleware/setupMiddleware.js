2dfb2341ec7698a0507ebb9f9a50fe250b7b92ab
Jay Fanse <jayf29112003@gmail.com>
2024-12-31 17:47:27 +0530

    changea headers

--- Backend/middleware/setupMiddleware.js
+++ Backend/middleware/setupMiddleware.js
@@ -2,38 +2,40 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import cors from 'cors';
-
+ 
 if(process.env.NODE_ENV !== "production"){
   (await import('dotenv')).config();
 }
-
-
+ 
+ 
 const configureApp = (app) => {
   app.set('trust proxy', 1);
-
+ 
   app.use(
-    cors({
-      origin: `${process.env.CLIENT_BASE_URL}`,
-      credentials: true, 
-      allowedHeaders: ['Content-Type', 'Authorization'], 
-    }
-  ));
-  
+  cors({
+    origin: "*",                // Allows requests from any origin
+    credentials: true,          // Allows cookies and HTTP authentication
+    allowedHeaders: ["Content-Type", "Authorization"] // Allow all headers
+  })
+);
+ 
+ 
   // Ensure preflight requests are handled
-  app.options('*', cors()); 
-  
-  app.use((req, res, next) => {
-    res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none'); 
-    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); 
+  app.options('*', cors());
+ 
+  app.use((req, r, next) => {
+    res.setHeesader('Cross-Origin-Opener-Policy', 'unsafe-none');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
     next();
 });
-
-
+ 
+ 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
   app.use(helmet());
-
+ 
 };
-
+ 
 export default configureApp;
+ 
