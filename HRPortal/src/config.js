d0db7f6aa6e6e40af03a94393d3c2c519845c84c
Arsh Vhora <arshvhora16@gmail.com>
2026-02-16 13:54:34 +0530

    Solved proxy error

--- HRPortal/src/config.js
+++ HRPortal/src/config.js
@@ -1,7 +1,7 @@
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000'
     // : 'https://hrportal.insansa.com';
-    :'https://hrportal.harshvaidya.online/';
+    :'https://hrportal.harshvaidya.online';
 
 
 const apibaseURl = "http://127.0.0.1:8000";
